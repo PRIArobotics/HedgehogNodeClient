a937d5e6a0260e29bd13141bfb743a1d2b63109e
SillyFreak <sillyfreak.ck@gmail.com>
2017-08-07 11:38:31 +0200

    add exports for protocol messages

--- hedgehog/index.ts
+++ hedgehog/index.ts
@@ -1,1 +1,2 @@
 export * from './client/hedgehogClient';
+export * from './protocol/index';
---
+++ hedgehog/protocol/index.ts
@@ -0,0 +1,12 @@
+import * as ack from './messages/ack';
+import * as io from './messages/io';
+import * as analog from './messages/analog';
+import * as digital from './messages/digital';
+import * as motor from './messages/motor';
+import * as servo from './messages/servo';
+import * as process from './messages/process';
+
+export {
+    ack, io, analog, digital,
+    motor, servo, process,
+};
