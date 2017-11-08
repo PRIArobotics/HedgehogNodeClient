60dcf9a2266ee2474d9b32a7df826d3273101ec4
Markus Klein <m@mklein.co.at>
2017-11-08 11:35:47 +0100

    update dependencies; remove babel and use ts only; add npm package lock file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,37 +2,10 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         ts: {
-            options: {
-                target: 'es6',
-                module: 'commonjs',
-                moduleResolution: 'node',
-                sourceMap: true,
-                emitDecoratorMetadata: true,
-                experimentalDecorators: true,
-                removeComments: false,
-                noImplicitAny: false,
-                allowJs: true
-            },
-            all: {
-                src: ['hedgehog/**/*.ts', 'typings/index.d.ts'],
-                outDir: 'build/hedgehog'
-            },
-            test: {
-                src: ['test/**/*.ts', 'typings/index.d.ts'],
-                outDir: 'build'
-            }
-        },
-        babel: {
-            options: {
-                presets: ['es2015']
-            },
-            all: {
-                files: [{
-                    expand: true,
-                    cwd: 'build',
-                    src: ['**/*.js', '!src/client/node_modules/*'],
-                    dest: 'build'
-                }]
+            default: {
+                tsconfig: {
+                    passThrough: true
+                }
             }
         },
         clean: [
@@ -66,13 +39,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-symlink');
-    grunt.loadNpmTasks('grunt-babel');
 
     grunt.registerTask('protoc', function() {
         var exec = require('child_process').execSync;
         exec("protoc --proto_path=proto --js_out=import_style=commonjs,binary:. `find proto -name '*.proto'`", { encoding: 'utf8' });
     });
 
-    grunt.registerTask('compile', ['ts', 'babel']);
-    grunt.registerTask('build', ['clean', 'compile', 'copy']);
+    grunt.registerTask('build', ['clean', 'ts', 'copy']);
 };
