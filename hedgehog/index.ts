ac5e01177117b177fedf7c472273aa7effb0e05f
SillyFreak <sillyfreak.ck@gmail.com>
2019-02-20 15:27:07 +0100

    add more missing exports

--- hedgehog/index.ts
+++ hedgehog/index.ts
@@ -1,5 +1,5 @@
 export * from './client/hedgehogClient';
-export { Message, ack, io, analog, digital, motor, servo, process } from './protocol';
+export { Message, ack, imu, io, analog, digital, motor, servo, process, speaker } from './protocol';
 
 import * as protocol from './protocol';
 export { protocol };
--- hedgehog/protocol/index.ts
+++ hedgehog/protocol/index.ts
@@ -1,11 +1,13 @@
 import * as ack from './messages/ack';
+import * as imu from './messages/imu';
 import * as io from './messages/io';
 import * as analog from './messages/analog';
 import * as digital from './messages/digital';
 import * as motor from './messages/motor';
 import * as servo from './messages/servo';
 import * as process from './messages/process';
+import * as speaker from './messages/speaker';
 
-export { ack, io, analog, digital, motor, servo, process };
+export { ack, imu, io, analog, digital, motor, servo, process, speaker };
 
 export { RequestMsg, ReplyMsg, Message, ContainerMessage } from './messages';
